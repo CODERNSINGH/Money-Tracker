5bff4045ef56311a2878a8ee3275c7a9db2cbc73
codernsingh <narendrasingh.coder@gmail.com>
2024-09-03 17:59:26 +0530

    ppt

---
+++ lec1.py
@@ -0,0 +1,3 @@
+a = int(input("Enter Your Number"))
+b = a % 2
+if (b == 0)
---
+++ try.py
@@ -0,0 +1,10 @@
+a = int(input("Enter No 1"))
+b = int(input("Enter No 1"))
+c = int(input("Enter No 1"))
+
+if (a > b){
+    print("a is Greater")
+}
+elif(b > a){
+    
+}
